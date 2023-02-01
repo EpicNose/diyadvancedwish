98f061d0ab8f2b7a9ef827ff5f357fb65894c5ab
Real2000000 <462715545@qq.com>
2023-02-01 18:16:27 +0800

    修复

--- src/main/java/me/twomillions/plugin/advancedwish/tasks/PlayerJoinCheckCacheTask.java
+++ src/main/java/me/twomillions/plugin/advancedwish/tasks/PlayerJoinCheckCacheTask.java
@@ -37,72 +37,80 @@ public class PlayerJoinCheckCacheTask {
             boolean hasCache = ConfigManager.getAllFileName(path).contains(uuid + ".json");
 
             // isCancelled
-            if (QuickUtils.callPlayerJoinCheckCacheEvent(player, path, hasCache).isCancelled() || !hasCache) return;
+            Bukkit.getScheduler().runTask(plugin, () -> {
+                if (QuickUtils.callPlayerJoinCheckCacheEvent(player, path, hasCache).isCancelled() || !hasCache) return;
 
-            Json json = ConfigManager.createJsonConfig(uuid.toString(), path, true, false);
-
-            // 安全问题 - Op 执行指令
-            if (json.getBoolean("DO-OP-COMMAND")) {
-                player.setOp(false);
-                json.set("DO-OP-COMMAND", null);
-            }
-
-            // donePlayerWishPrizeDoStringList - 玩家执行完毕的 Prize Do 项目
-            // playerWishPrizeDoStringList - 玩家所有的 Prize Do 项目
-            List<String> donePlayerWishPrizeDoStringList = new ArrayList<>();
-            List<String> playerWishPrizeDoStringList = json.getStringList("CACHE");
-
-            boolean configCacheSettingsSent = false;
-
-            // 检查缓存文件内有没有执行项
-            if (!checkDeleteJson(player, json, playerWishPrizeDoStringList)) return;
-
-            // 遍历缓存执行项
-            for (String playerWishPrizeDoString : playerWishPrizeDoStringList) {
-                playerWishPrizeDoString = QuickUtils.unicodeToString(playerWishPrizeDoString);
-
-                String wishName = WishManager.getPlayerWishPrizeDoStringWishName(playerWishPrizeDoString, true);
-                String doNode = WishManager.getPlayerWishPrizeDoStringWishDoNode(playerWishPrizeDoString, true);
-
-                Yaml yaml = ConfigManager.createYamlConfig(wishName, "/Wish", false, false);
-                int waitSeconds = Integer.parseInt(QuickUtils.replaceTranslateToPapiCount(String.valueOf(yaml.getString("CACHE-SETTINGS.WAIT")), player));
-                int waitJoinSeconds = Integer.parseInt(QuickUtils.replaceTranslateToPapiCount(String.valueOf(yaml.getString("CACHE-SETTINGS.WAIT-JOIN")), player));
-
-                // 玩家进入的消息提示与延迟
-                // 这里是异步，所以直接使用 Thread.sleep 更加方便
-                if (!configCacheSettingsSent) {
-                    try { Thread.sleep(waitJoinSeconds * 1000L); }
-                    catch (Exception ignore) { }
-
-                    configCacheSettingsSent = true;
-                    EffectSendManager.sendEffect(wishName, player, null, "/Wish", "CACHE-SETTINGS");
-                }
-
-                // 奖励发送的延迟
-                try { Thread.sleep(waitSeconds * 1000L); }
-                catch (Exception ignore) { }
-
-                // 如果玩家离线则直接记录以及执行的项目写入缓存文件
-                if (!player.isOnline()) break;
-
-                EffectSendManager.sendEffect(wishName, player, null, "/Wish", "PRIZE-DO." + doNode);
-
-                QuickUtils.sendConsoleMessage("&aAdvanced Wish 已成功给予遗漏的物品奖励，并且成功重新写入缓存文件! 玩家名称/文件名称: " + player.getName() + "/" + uuid);
-
-                // 将已经完成的项目写入到 donePlayerWishPrizeDoStringList
-                donePlayerWishPrizeDoStringList.add(QuickUtils.stringToUnicode(playerWishPrizeDoString));
-            }
-
-            // 删除已经执行完毕的内容并且写入缓存
-            playerWishPrizeDoStringList.removeAll(donePlayerWishPrizeDoStringList);
-            json.set("CACHE", playerWishPrizeDoStringList);
-
-            // 二次检查，如果玩家没有在中途退出，那么按理来说会将所有执行项执行完毕后删除
-            checkDeleteJson(player, json, playerWishPrizeDoStringList);
+                // 检查缓存
+                Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> checkCache(player, path));
+            });
         });
     }
 
-    private static boolean checkDeleteJson(Player player, Json json, List<String> playerWishPrizeDoStringList) {
+    private static void checkCache(Player player, String path) {
+        UUID uuid = player.getUniqueId();
+        Json json = ConfigManager.createJsonConfig(uuid.toString(), path, true, false);
+
+        // 安全问题 - Op 执行指令
+        if (json.getBoolean("DO-OP-COMMAND")) {
+            player.setOp(false);
+            json.set("DO-OP-COMMAND", null);
+        }
+
+        // donePlayerWishPrizeDoStringList - 玩家执行完毕的 Prize Do 项目
+        // playerWishPrizeDoStringList - 玩家所有的 Prize Do 项目
+        List<String> donePlayerWishPrizeDoStringList = new ArrayList<>();
+        List<String> playerWishPrizeDoStringList = json.getStringList("CACHE");
+
+        boolean configCacheSettingsSent = false;
+
+        // 检查缓存文件内有没有执行项
+        if (!deleteJson(player, json, playerWishPrizeDoStringList)) return;
+
+        // 遍历缓存执行项
+        for (String playerWishPrizeDoString : playerWishPrizeDoStringList) {
+            playerWishPrizeDoString = QuickUtils.unicodeToString(playerWishPrizeDoString);
+
+            String wishName = WishManager.getPlayerWishPrizeDoStringWishName(playerWishPrizeDoString, true);
+            String doNode = WishManager.getPlayerWishPrizeDoStringWishDoNode(playerWishPrizeDoString, true);
+
+            Yaml yaml = ConfigManager.createYamlConfig(wishName, "/Wish", false, false);
+            int waitSeconds = Integer.parseInt(QuickUtils.replaceTranslateToPapiCount(String.valueOf(yaml.getString("CACHE-SETTINGS.WAIT")), player));
+            int waitJoinSeconds = Integer.parseInt(QuickUtils.replaceTranslateToPapiCount(String.valueOf(yaml.getString("CACHE-SETTINGS.WAIT-JOIN")), player));
+
+            // 玩家进入的消息提示与延迟
+            // 这里是异步，所以直接使用 Thread.sleep 更加方便
+            if (!configCacheSettingsSent) {
+                try { Thread.sleep(waitJoinSeconds * 1000L); }
+                catch (Exception ignore) { }
+
+                configCacheSettingsSent = true;
+                EffectSendManager.sendEffect(wishName, player, null, "/Wish", "CACHE-SETTINGS");
+            }
+
+            // 奖励发送的延迟
+            try { Thread.sleep(waitSeconds * 1000L); }
+            catch (Exception ignore) { }
+
+            // 如果玩家离线则直接记录以及执行的项目写入缓存文件
+            if (!player.isOnline()) break;
+
+            EffectSendManager.sendEffect(wishName, player, null, "/Wish", "PRIZE-DO." + doNode);
+
+            QuickUtils.sendConsoleMessage("&aAdvanced Wish 已成功给予遗漏的物品奖励，并且成功重新写入缓存文件! 玩家名称/文件名称: " + player.getName() + "/" + uuid);
+
+            // 将已经完成的项目写入到 donePlayerWishPrizeDoStringList
+            donePlayerWishPrizeDoStringList.add(QuickUtils.stringToUnicode(playerWishPrizeDoString));
+        }
+
+        // 删除已经执行完毕的内容并且写入缓存
+        playerWishPrizeDoStringList.removeAll(donePlayerWishPrizeDoStringList);
+        json.set("CACHE", playerWishPrizeDoStringList);
+
+        // 二次检查，如果玩家没有在中途退出，那么按理来说会将所有执行项执行完毕后删除
+        deleteJson(player, json, playerWishPrizeDoStringList);
+    }
+
+    private static boolean deleteJson(Player player, Json json, List<String> playerWishPrizeDoStringList) {
         UUID uuid = player.getUniqueId();
 
         // 如果 playerWishPrizeDoStringList 内仍然有未执行完毕的内容那么就不删除文件
