bbf88231676d566e16184064a144a80099ca8a46
2000000 <462715545@qq.com>
2023-03-07 19:56:12 +0800

    Constants.java ".yaml" to ".yml"

--- src/main/java/me/twomillions/plugin/advancedwish/Constants.java
+++ src/main/java/me/twomillions/plugin/advancedwish/Constants.java
@@ -13,5 +13,5 @@ public class Constants {
     public static final String PLAYER_LOGS = "/PlayerLogs";
     
     public static final String JSON = ".json";
-    public static final String YAML = ".yaml";
+    public static final String YAML = ".yml";
 }
